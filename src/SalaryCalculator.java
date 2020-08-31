604bd7526e28682725a83756037c2a4a354797b9
Ashok kumar K <52928917+admin-xonlabs@users.noreply.github.com>
2020-08-31 15:17:31 +0530

    Add files via upload

---
+++ src/SalaryCalculator.java
@@ -0,0 +1,23 @@
+
+public class SalaryCalculator {
+
+	public static double computeInHandSalary(int numberOfWorkedHours, double basePay) {
+		double salary = 0;
+
+		// Your Logic goes here
+
+		return salary;
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(computeInHandSalary(80, 150000));
+		System.out.println(computeInHandSalary(160, 200000));
+		System.out.println(computeInHandSalary(175, 300000));
+		System.out.println(computeInHandSalary(200, 180000));
+		System.out.println(computeInHandSalary(220, 140000));
+		System.out.println(computeInHandSalary(240, 190000));
+
+	}
+
+}
