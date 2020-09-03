0b5754cdf0b28e0a2288f564424f5667fb5e492f
Ashok kumar K <52928917+admin-xonlabs@users.noreply.github.com>
2020-09-03 16:08:29 +0530

    Add files via upload

---
+++ CodeChallenge4/src/com/xonlabs/servlet/TableServlet.java
@@ -0,0 +1,29 @@
+package com.xonlabs.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TableServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		pw.print("Welcome to the world of Servlet Programming");
+
+		pw.close();
+	}
+
+}
