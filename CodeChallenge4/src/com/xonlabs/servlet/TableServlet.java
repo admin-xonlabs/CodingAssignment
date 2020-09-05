5510f9746636c2fb168e7a669f3d24a99e0cd5b6
TejasGowdaS <tejasgowda713@gmail.com>
2020-09-05 18:13:02 +0530

    Assignment 4 completed by Team- TechnoStars

--- CodeChallenge4/src/com/xonlabs/servlet/TableServlet.java
+++ CodeChallenge4/src/com/xonlabs/servlet/TableServlet.java
@@ -20,9 +20,29 @@ public class TableServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
+		String row = (req.getParameter("rowSize"));
+		String col = (req.getParameter("colSize"));
 		PrintWriter pw = resp.getWriter();
-		pw.print("Welcome to the world of Servlet Programming");
-
+		
+		
+		pw.print("<div style='background-color: #444' >");
+		pw.print("<table style='border: 1px solid black' width='100%'>");
+		for(int i=0; i<Integer.parseInt(row); i++) {
+			pw.print("<tr >");
+			for(int j=0; j < Integer.parseInt(col); j++) {
+				if(i%2 == 0) {
+				pw.print("<td style='background-color:#eb8f8f' align='center' >"+"Row "+(i+1)+", Col "+(j+1)+"</td>");
+				} else {
+					pw.print("<td style='background-color:#40a8c4' align='center'>"+"Row "+(i+1)+", Col "+(j+1)+"</td>");
+				}
+				
+			}
+			pw.print("</tr>");
+		}
+		
+		pw.print("</table>");
+		pw.print("</div>");
+		pw.print(" Thank You!! <br> Done by Team - TechnoStars* ");
 		pw.close();
 	}
 
