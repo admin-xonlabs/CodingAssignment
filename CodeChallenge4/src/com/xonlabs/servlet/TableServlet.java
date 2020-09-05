e897faab6bd4a772e424f8d74e0f9bd451e9273f
TejasGowdaS <tejasgowda713@gmail.com>
2020-09-05 18:56:52 +0530

    Coding Assignment 4 completed by Team TechnoStars

--- CodeChallenge4/src/com/xonlabs/servlet/TableServlet.java
+++ CodeChallenge4/src/com/xonlabs/servlet/TableServlet.java
@@ -20,16 +20,23 @@ public class TableServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
-		String row = (req.getParameter("rowSize"));
-		String col = (req.getParameter("colSize"));
 		PrintWriter pw = resp.getWriter();
+		try {
+		int row = Integer.parseInt(req.getParameter("rowSize"));
+		int col = Integer.parseInt(req.getParameter("colSize"));
 		
+		
+		if(row ==0 || col==0) {
+			pw.write("Row or Column value cannot be zero.. ");
+		}
+		
+		else if(row==(int)row && col==(int)col) {
 		
 		pw.print("<div style='background-color: #444' >");
 		pw.print("<table style='border: 1px solid black' width='100%'>");
-		for(int i=0; i<Integer.parseInt(row); i++) {
+		for(int i=0; i<row; i++) {
 			pw.print("<tr >");
-			for(int j=0; j < Integer.parseInt(col); j++) {
+			for(int j=0; j < col; j++) {
 				if(i%2 == 0) {
 				pw.print("<td style='background-color:#eb8f8f' align='center' >"+"Row "+(i+1)+", Col "+(j+1)+"</td>");
 				} else {
@@ -42,7 +49,13 @@ public class TableServlet extends HttpServlet {
 		
 		pw.print("</table>");
 		pw.print("</div>");
-		pw.print(" Thank You!! <br> Done by Team - TechnoStars* ");
+		}
+		}
+		
+		catch(Exception e) {
+			pw.print("Please Enter a valid input xD ");
+		}
+		pw.print(" Thank You!! <br> Created by Team - TechnoStars* ");
 		pw.close();
 	}
 
